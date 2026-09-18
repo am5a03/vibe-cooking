fdeaaf40a0cbb1a24587f67a8b434b0af5abddc8
Raymond Chan <am5a03@gmail.com>
2026-09-18 00:05:54 -0700

    chore: add Drizzle Kit config

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './db/schema.ts',
+  out: './drizzle/migrations',
+  dialect: 'sqlite',
+  strict: true,
+  verbose: true,
+});
